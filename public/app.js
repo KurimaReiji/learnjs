8cf07331ba09ede27abd49d6309f4d1474d45580
Kurima Reiji <KurimaReiji@kurimail.com>
2017-07-02 22:26:07 +0900

    end of 3.4.1, P69

--- public/app.js
+++ public/app.js
@@ -26,10 +26,11 @@ learnjs.problemView = function(data){
   }
 
   function checkAnswerClick(){
-    if( checkAnswer()){
-      resultFlash.text('Correct!')
+    if(checkAnswer()){
+      var correctFlash = learnjs.buildCorrectFlash(problemNumber)
+      learnjs.flashElement(resultFlash, correctFlash)
     }else{
-      resultFlash.text('Incorrect!')      
+      learnjs.flashElement(resultFlash, 'Incorrect!')
     }
     return false
   }
@@ -42,7 +43,8 @@ learnjs.problemView = function(data){
 
 learnjs.showView = function(hash){
   var routes = {
-    '#problem': learnjs.problemView
+    '#problem': learnjs.problemView,
+    '': learnjs.landingView
   }
   var hashParts = hash.split("-")
   var viewFn = routes[hashParts[0]]
@@ -62,4 +64,31 @@ learnjs.applyObject = function(obj, elem){
   for ( var key in obj) {
     elem.find('[data-name="' + key + '"]').text(obj[key])
   }
+}
+
+learnjs.flashElement = function(elem, content){
+  elem.fadeOut('fast', function(){
+    elem.html(content)
+    elem.fadeIn()
+  })
+}
+
+learnjs.template = function(name){
+  return $('.templates .' + name).clone()
+}
+
+learnjs.buildCorrectFlash = function(problemNum){
+  var correctFlash = learnjs.template('correct-flash')
+  var link = correctFlash.find('a')
+  if (problemNum < learnjs.problems.length) {
+    link.attr('href', '#problem-' + (problemNum + 1))
+  }else{
+    link.attr('href','')
+    link.text("You're Finished!")
+  }
+  return correctFlash
+}
+
+learnjs.landingView = function(){
+  return learnjs.template('landing-view')
 }
