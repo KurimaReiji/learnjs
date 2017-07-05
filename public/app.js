073799349a31482114274a0245476406f7e9e1cd
Kurima Reiji <KurimaReiji@kurimail.com>
2017-07-05 16:42:08 +0900

    end of chap3

--- public/app.js
+++ public/app.js
@@ -35,6 +35,15 @@ learnjs.problemView = function(data){
     return false
   }
 
+  if (problemNumber < learnjs.problems.length){
+    var buttonItem = learnjs.template('skip-btn')
+    buttonItem.find('a').attr('href', '#problem-' + (problemNumber + 1))
+    $('.nav-list').append(buttonItem)
+    view.bind('removingView', function(){
+      buttonItem.remove()
+    })
+  }
+
   view.find('.check-btn').click(checkAnswerClick)
   view.find('.title').text('Problem #' + problemNumber)
   learnjs.applyObject(problemData, view)
@@ -44,11 +53,13 @@ learnjs.problemView = function(data){
 learnjs.showView = function(hash){
   var routes = {
     '#problem': learnjs.problemView,
+    '#': learnjs.landingView,
     '': learnjs.landingView
   }
   var hashParts = hash.split("-")
   var viewFn = routes[hashParts[0]]
   if (viewFn) {
+    learnjs.triggerEvent('removingView',[])
     $(".view-container").empty().append(viewFn(hashParts[1]))
   }
 }
@@ -91,4 +102,8 @@ learnjs.buildCorrectFlash = function(problemNum){
 
 learnjs.landingView = function(){
   return learnjs.template('landing-view')
+}
+
+learnjs.triggerEvent = function(name, args){
+  $('.view-container>*').trigger(name, args)
 }
